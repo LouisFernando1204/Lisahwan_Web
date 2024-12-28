dae44ddfa04b264797bafbf2c7d9c84c901b18d0
Louis Fernando <ferlouis55@gmail.com>
2024-12-28 23:27:11 +0700

    Add toggle visibility functionality for total price in order history & order today page for both admin & owner

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -154,11 +154,32 @@
                         </form>
 
                         <span data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="800"
-                            class="mt-2 lg:mt-0 flex flex-row justify-center items-center bg-green-300 text-green-900 text-sm text-center font-medium px-3 py-2 rounded-full">
-                            <span class="w-2 h-2 me-2 bg-green-500 rounded-full"></span>
-                            Total Penjualan:
-                            Rp. {{ $totalPrice }}
-                        </span>
+                        class="mt-2 lg:mt-0 flex flex-row justify-center items-center bg-green-300 text-green-900 text-sm text-center font-medium px-3 py-2 rounded-full">
+                        <span class="w-2 h-2 me-2 bg-green-500 rounded-full"></span>
+                        <div id="price-container"
+                            class="w-full flex flex-row justify-center items-center text-center">
+                            <span class="mr-2 text-center">Total Penjualan:</span>
+                            <span id="status-placeholder" class="block text-center">●●●●●●</span>
+                            <span id="total-price" class="hidden text-center">Rp. {{ $totalPrice }}</span>
+                        </div>
+                        <button id="toggle-visibility" class="ml-2 bg-green-500 rounded-full p-1">
+                            <svg id="icon-eye-closed" class="w-6 h-6 text-green-900"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path
+                                    d="m4 15.6 3.055-3.056A4.913 4.913 0 0 1 7 12.012a5.006 5.006 0 0 1 5-5c.178.009.356.027.532.054l1.744-1.744A8.973 8.973 0 0 0 12 5.012c-5.388 0-10 5.336-10 7A6.49 6.49 0 0 0 4 15.6Z" />
+                                <path
+                                    d="m14.7 10.726 4.995-5.007A.998.998 0 0 0 18.99 4a1 1 0 0 0-.71.305l-4.995 5.007a2.98 2.98 0 0 0-.588-.21l-.035-.01a2.981 2.981 0 0 0-3.584 3.583c0 .012.008.022.01.033.05.204.12.402.12.59l-4.995 4.983a1 1 0 1 0 1.414 1.414l4.995-4.983c.189.091.386.162.59.211.011 0 .021.007.033.01a2.982 2.982 0 0 0 3.584-3.584c0-.012-.008-.023-.011-.035a3.05 3.05 0 0 0-.21-.588Z" />
+                                <path
+                                    d="m19.821 8.605-2.857 2.857a4.952 4.952 0 0 1-5.514 5.514l-1.785 1.785c.767.166 1.55.25 2.335.251 6.453 0 10-5.258 10-7 0-1.166-1.637-2.874-2.179-3.407Z" />
+                            </svg>
+                            <svg id="icon-eye-open" class="hidden w-6 h-6 text-green-900"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M4.998 7.78C6.729 6.345 9.198 5 12 5c2.802 0 5.27 1.345 7.002 2.78a12.713 12.713 0 0 1 2.096 2.183c.253.344.465.682.618.997.14.286.284.658.284 1.04s-.145.754-.284 1.04a6.6 6.6 0 0 1-.618.997 12.712 12.712 0 0 1-2.096 2.183C17.271 17.655 14.802 19 12 19c-2.802 0-5.27-1.345-7.002-2.78a12.712 12.712 0 0 1-2.096-2.183 6.6 6.6 0 0 1-.618-.997C2.144 12.754 2 12.382 2 12s.145-.754.284-1.04c.153-.315.365-.653.618-.997A12.714 12.714 0 0 1 4.998 7.78ZM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </span>
                     </div>
                 </div>
                 <div class="overflow-x-auto overflow-y-hidden">
@@ -824,5 +845,36 @@
             </div>
         </div>
     </section>
+    <script language="javascript">
+        document.getElementById("toggle-visibility").addEventListener("click", function() {
+            const priceContainer = document.getElementById("price-container");
+            const statusPlaceholder = document.getElementById("status-placeholder");
+            const totalPrice = document.getElementById("total-price");
+            const iconEyeClosed = document.getElementById("icon-eye-closed");
+            const iconEyeOpen = document.getElementById("icon-eye-open");
+
+            if (!statusPlaceholder.classList.contains("hidden")) {
+                statusPlaceholder.classList.add("hidden");
+                totalPrice.classList.remove("hidden");
+
+                iconEyeClosed.classList.add("hidden");
+                iconEyeOpen.classList.remove("hidden");
+
+                if (window.innerWidth < 640) {
+                    priceContainer.classList.replace("flex-row", "flex-col");
+                } else {
+                    priceContainer.classList.replace("flex-col", "flex-row");
+                }
+            } else {
+                statusPlaceholder.classList.remove("hidden");
+                totalPrice.classList.add("hidden");
+
+                iconEyeOpen.classList.add("hidden");
+                iconEyeClosed.classList.remove("hidden");
+
+                priceContainer.classList.replace("flex-col", "flex-row");
+            }
+        });
+    </script>
     <!-- End block -->
 @endsection
